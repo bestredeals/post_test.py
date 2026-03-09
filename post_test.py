3888982010b39146b5743118cd28ea5b605aade6
bestredeals <saly365@gmail.com>
2026-03-09 11:06:14 -0500

    Update post_test.py

--- post_test.py
+++ post_test.py
@@ -1,15 +1,20 @@
 import os
 import requests
 
-BOT_TOKEN = os.environ["8557388882:AAEj4puUuzPe_IaYQatP_B1h4P610q5j2Lg"]
+BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = "@bestredeals"
 
 message = "✅ Automation is working"
 
-requests.post(
-    f"https://api.telegram.org/bot8557388882:AAEj4puUuzPe_IaYQatP_B1h4P610q5j2Lg/sendMessage",
+response = requests.post(
+    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
     data={
         "chat_id": CHAT_ID,
         "text": message
-    }
+    },
+    timeout=30
 )
+
+print(response.status_code)
+print(response.text)
+response.raise_for_status()
