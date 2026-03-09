c911d65085561aa8d59c6c861fbb08dfa10e3876
bestredeals <saly365@gmail.com>
2026-03-09 11:10:07 -0500

    Update post_test.py

--- post_test.py
+++ post_test.py
@@ -1,20 +1,38 @@
 import os
 import requests
+import xml.etree.ElementTree as ET
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = "@bestredeals"
 
-message = "✅ Automation is working"
+RSS_URL = "https://slickdeals.net/newsearch.php?mode=frontpage&searcharea=deals&rss=1"
 
-response = requests.post(
-    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-    data={
-        "chat_id": CHAT_ID,
-        "text": message
-    },
-    timeout=30
-)
+def send_message(text):
+    requests.post(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        data={
+            "chat_id": CHAT_ID,
+            "text": text
+        }
+    )
 
-print(response.status_code)
-print(response.text)
-response.raise_for_status()
+rss = requests.get(RSS_URL)
+root = ET.fromstring(rss.content)
+
+items = root.findall(".//item")
+
+for item in items[:3]:
+
+    title = item.find("title").text
+    link = item.find("link").text
+
+    message = f"""
+🔥 DEAL ALERT
+
+{title}
+
+See deal:
+{link}
+"""
+
+    send_message(message)
